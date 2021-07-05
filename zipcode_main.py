233d2e0f24c14b1b04c358b22a84c69e8ccad77c
EC2 Default User <ec2-user@ip-172-31-31-234.us-east-2.compute.internal>
2021-07-05 13:10:38 +0000

    更新代码

---
+++ zipcode_main.py
@@ -0,0 +1,10 @@
+import geopandas as gpd
+from shapely import wkt
+# read in file from shapefile or other format using geopandas
+gdf = gpd.read_file('zip://tl_2019_us_zcta510.zip')
+#gdf = gdf.to_crs(4326)
+gdf['geometry'] = gdf.geometry.apply(lambda x: wkt.dumps(x))
+# call .to_geoparquet() method on geopandas GeoDataFrame to write to file
+gdf.to_parquet('tl_2019_us_zcta510.parquet',compression=None)
+#gdf.to_file('dataframe.geojson', driver='GeoJSON')
+
